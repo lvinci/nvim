793edf4acbd488aa40b80e5f358f351468fc5b3b
Luca Vinciguerra <lvinci@outlook.de>
2024-01-12 16:47:48 +0100

    add lsp confirm keybinding

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -30,7 +30,6 @@ return {
 
       -- And you can configure cmp even more, if you want to.
       local cmp = require('cmp')
-      local cmp_action = lsp_zero.cmp_action()
 
       cmp.setup({
         formatting = lsp_zero.cmp_format(),
@@ -38,8 +37,7 @@ return {
           ['<C-Space>'] = cmp.mapping.complete(),
           ['<C-u>'] = cmp.mapping.scroll_docs(-4),
           ['<C-d>'] = cmp.mapping.scroll_docs(4),
-          ['<C-f>'] = cmp_action.luasnip_jump_forward(),
-          ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+          ['<CR>'] = cmp.mapping.confirm({ select = true }),
         })
       })
     end
